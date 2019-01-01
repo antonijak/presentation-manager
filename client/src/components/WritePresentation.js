43d09277b4faef52a9d2af9ab68463d4698318b6
Antonija K <antonija.kasum@gmail.com>
2019-01-01 22:55:46 +0200

    WritePresentation form layout

--- client/src/components/WritePresentation.js
+++ client/src/components/WritePresentation.js
@@ -1,7 +1,107 @@
 import React from "react";
+import "./WritePresentation.scss";
 
 const WritePresentation = () => {
-  return <div>Write Presentation</div>;
+  return (
+    <div className="container mt-4">
+      <h2>New Presentation</h2>
+      <form>
+        <div className="form-row">
+          <div className="form-group col-md-4">
+            <label htmlFor="form-presenter">Presenter*</label>
+            <input
+              type="text"
+              className="form-control form__presenter"
+              id="form-presenter"
+              placeholder="Full Name"
+            />
+          </div>
+
+          <div className="form-group col-md-4">
+            <label htmlFor="form-evaluator">Evaluator*</label>
+            <input
+              type="text"
+              className="form-control form__evaluator"
+              id="form-evaluator"
+              placeholder="Full Name"
+            />
+          </div>
+
+          <div className="form-group col-md-4">
+            <label htmlFor="form-date">Date*</label>
+            <input
+              type="date"
+              className="form-control form__date"
+              id="form-date"
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="form-topic">Topic*</label>
+          <input
+            type="text"
+            className="form-control form__topic"
+            id="form-topic"
+            placeholder="Presentation Topic"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="form-article1">Article 1*</label>
+          <input
+            type="url"
+            className="form-control form__article"
+            id="form-article1"
+            placeholder="www.some-link.com"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="form-article2">Article 2</label>
+          <input
+            type="url"
+            className="form-control form__article"
+            id="form-article2"
+            placeholder="www.some-link.com"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="form-article3">Article 3</label>
+          <input
+            type="url"
+            className="form-control form__article"
+            id="form-article3"
+            placeholder="www.some-link.com"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="form-summary">Summary*</label>
+          <textarea
+            className="form-control form__summary"
+            id="form-summary"
+            rows="3"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="form-keywords">Keywords</label>
+          <input
+            type="url"
+            className="form-control form__keywords"
+            id="form-keywords"
+            placeholder="eg. learning, coding"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary form__submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default WritePresentation;
